d1be3e9ec72ab26203148e45a52e5f77e7fec4d4
LYJ0304 <omlmg0304@naver.com>
2026-09-20 09:52:10 +0000

    chore: add dependencies and refactoring file's path

--- src/main/java/AuthServiceApplication.java
+++ src/main/java/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.authservice;
+package authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/AuthServiceApplicationTests.java
+++ src/test/java/AuthServiceApplicationTests.java
@@ -1,12 +1,14 @@
-package com.example.authservice;
+package authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() { 
     }
 }
